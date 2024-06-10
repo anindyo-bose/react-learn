270c79be8567ddc4a2316ae899d3295e6ba7518d
Anindyo Bose <boseanindyo05@gmail.com>
2024-06-10 22:29:31 +0530

    Added Header wireframe

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/**
+ * Header component
+ *  - Logo
+ *  - Navigation
+ * Body
+ *  - Search
+ *  - ResturantContainer
+ *    - ResturantCard
+ * Footer
+ *  - CopyRight
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
 
-// React way of creating element
-const heading = React.createElement('h1', {id: 'Head'}, "Anindyo Bose");
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logoContainer'>
+                <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjJ8giiCmiHq5_yA_zwouIXEt0mkAkDyM-ww&s' alt='logo' />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )};
 
-// JSX way of creating element
-const jsx = <h1 id="Head">Anindyo Bose JSX</h1>;
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsx);
+root.render(<AppLayout/>);
