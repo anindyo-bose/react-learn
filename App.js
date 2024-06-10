8a58f8a6a7f7e53dda9daa692b0fadfe0ac7dd7d
Anindyo Bose <boseanindyo05@gmail.com>
2024-06-10 23:28:55 +0530

    SAve

--- App.js
+++ App.js
@@ -9,6 +9,10 @@ import ReactDOM from 'react-dom';
  *  - Search
  *  - ResturantContainer
  *    - ResturantCard
+ *    - Image
+ *    - Name
+ *    - Rating
+ *    - Address
  * Footer
  *  - CopyRight
  *  - Links
@@ -33,10 +37,40 @@ const Header = () => {
         </div>
     )};
 
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>
+                <input type='text' placeholder='Search for resturants' />
+                <button>Search</button>
+            </div>
+            <div className='resturantContainer'>
+                <ResturantCard resName="Meghana Foods" cuisin="North Indian, Biryani, Asian"/>
+                <ResturantCard resName="KFC" cuisin="Burger, Fast food"/>
+            </div>
+        </div>
+    );
+}
+
+const ResturantCard = (props) => {
+    return (
+        <div className='resturantCard'>
+            <img className='resturantImage' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjJ8giiCmiHq5_yA_zwouIXEt0mkAkDyM-ww&s' alt='resturant' />
+            <div className='resturantDetails'>
+                <h3>{props.resName}</h3>
+                <h4>{props.cuisin}</h4>
+                <h4>Rating: 4.5</h4>
+                <h4>30 minutes</h4>
+            </div>
+        </div>
+    );
+}
+
 const AppLayout = () => {
     return (
         <div className='app'>
             <Header />
+            <Body />
         </div>
     );
 };
